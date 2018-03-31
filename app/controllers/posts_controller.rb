c156abfa9f6c555d9fc1e9c2bb63edd3c029fb1a
John Ivan Puayap <johnivanpuayap@gmail.com>
2018-03-31 14:13:10 +0800

    Added the needed functionalities

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,35 @@
 class PostsController < ApplicationController
-
+	before_action :find_post, only: [:show, :edit, :update, :destroy]
 	def index
-
+		@post = Post.new
+		@posts = Post.search(params[:search])
 	end
+
+	def new
+		@post = Post.new
+	end
+
+	def create
+		@post = Post.new(post_params)
+
+		if @post.save
+			redirect_to @post
+		else
+			render 'new'
+		end
+	end
+
+	def show
+		
+	end
+
+
+	private
+
+		def find_post
+			@post = Post.find(params[:id])
+		end
+		def post_params
+			params.require(:post).permit(:author, :content, :search)
+		end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,11 @@
 class Post < ActiveRecord::Base
+	def self.search(term)
+			if term != 'Anonymous' and term != ''
+				where('author LIKE?', "%#{term}%").order('id DESC')
+			elsif term == 'Anonymous'
+				where('author LIKE?', "").order('id DESC')
+			else
+				Post.all.order("created_at DESC")
+		end
+	end
 end
